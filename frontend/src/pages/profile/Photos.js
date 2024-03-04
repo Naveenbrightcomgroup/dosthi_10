b3836d3327e343609266d0076357896b44c84b65
Naveenbrightcomgroup <naveenkumar.r@brightcomgroup>
2024-03-04 17:51:43 +0530

    created

---
+++ frontend/src/pages/profile/Photos.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import { photosReducer } from "../../functions/reducers";
+
+export default function Photos({ username, token, showPhotos, setshowPhotos }) {
+  const [{ loading, error, photos }, dispatch] = useReducer(photosReducer, {
+    loading: false,
+    photos: {},
+    error: "",
+  });
+  useEffect(() => {
+    getPhotos();
+  }, [username]);
+  const path = `${username}/*`;
+  const max = 30;
+  const sort = "desc";
+  const showGridImages = () => {
+    setshowPhotos(!showPhotos)
+  }
+  const getPhotos = async () => {
+    try {
+      dispatch({
+        type: "PHOTOS_REQUEST",
+      });
+      const { data } = await axios.post(
+        `/api1/listImages`,
+        { path, sort, max },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      dispatch({
+        type: "PHOTOS_SUCCESS",
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "PHOTOS_ERROR",
+        payload: error.response.data.message,
+      });
+    }
+  };
+  console.log("---->", photos);
+  return (
+    <div className="profile_card">
+      <div className="profile_card_header">
+        Photos
+        <div className="profile_header_link" onClick={showGridImages}>
+          See All Photos
+        </div>
+      </div>
+      <div className="profile_card_count">
+        {photos && photos.total_count === 0
+          ? ""
+          : photos.total_count === 1
+            ? "1 Photo"
+            : `${photos.total_count} photos`}
+      </div>
+      <div className="profile_card_grid">
+        {photos && photos.resources &&
+          photos.resources.slice(0, 3, photos.total_count).map((img) => (
+            <div className="profile_photo_card" key={img.public_id}>
+              <img src={img.secure_url} alt="" />
+            </div>
+          ))}
+      </div>
+      {showPhotos && (
+        <div className="profile_card_grid">
+          {photos && photos.resources &&
+            photos.resources.slice(3, photos.total_count).map((img) => (
+              <div className="profile_photo_card" key={img.public_id}>
+                <img src={img.secure_url} alt="" />
+              </div>
+            ))}
+        </div>
+
+      )}
+    </div>
+  );
+}
